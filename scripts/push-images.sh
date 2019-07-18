2073b676f8266263e6bfe8b8296582b64660ba79
Kieran Hester <khester@worth.systems>
2019-07-18 07:38:18 +0100

    remove echo

--- scripts/push-images.sh
+++ scripts/push-images.sh
@@ -6,10 +6,6 @@ NEXUS_USERNAME=$1
 NEXUS_PASSWORD=$2
 NEXUS_REGISTRY=$3
 
-echo ${NEXUS_USERNAME}
-echo ${NEXUS_PASSWORD}
-echo ${NEXUS_REGISTRY}
-
 docker login -u ${NEXUS_USERNAME} -p ${NEXUS_PASSWORD} ${NEXUS_REGISTRY}
 
 echo "Pushing app base image"
